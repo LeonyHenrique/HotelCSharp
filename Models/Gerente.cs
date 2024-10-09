4d593becfcd22b64d22b4b3ab35f91435ac5835d
leony <leo.ny2010@hotmail.com>
2024-10-09 17:00:03 -0300

    criando o hotelCSharp

---
+++ Models/Gerente.cs
@@ -0,0 +1,21 @@
+
+namespace HotelCSharp.Models
+{
+    public class Gerente : Pessoa
+    {
+        public Gerente() 
+        : base()
+        {
+        }
+
+        public Gerente(string nome, string cpf, string telefone) 
+        : base(nome, cpf, telefone)
+        {
+        }
+
+        public override void SeApresentar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("*******************************************");
+Console.WriteLine("*                                         *");
+Console.WriteLine("*      WELCOME TO THE GRAND HOTEL C#      *");
+Console.WriteLine("*                                         *");
+Console.WriteLine("*******************************************");
+Console.WriteLine("*              ______                     *");
+Console.WriteLine("*             |      |    HOTEL           *");
+Console.WriteLine("*      _______|______|_______             *");
+Console.WriteLine("*     |                     |             *");
+Console.WriteLine("*     |   []    []   []     |             *");
+Console.WriteLine("*     |                     |             *");
+Console.WriteLine("*     |_____________________|             *");
+Console.WriteLine("*******************************************");
+Console.WriteLine();
